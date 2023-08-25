50a2c8d606c5f3461c2d0a3c07cf9488e7637688
tloeber <thomas.loeber73@gmail.com>
2023-08-25 09:18:16 -0400

    Use enums and virtual subclasses to model dataset format.

--- src/oo_ml/interface/data/format.py
+++ src/oo_ml/interface/data/format.py
@@ -1,41 +1,49 @@
+from abc import ABC
 from enum import Enum
-from typing import Generic, TypeAlias
 
 import pandas as pd
 import numpy as np
 
 
-# class BaseDataFormat(Enum):
-#     """
-#     The fact that this enum does not have any attributes/members makes it
-#     abstract, since it is thus not possible to instantiate it.
-#     While more direct ways of designating this class as an abstractclass, e.g.
-#     using abc.ABC or abc.abstractmethod would be preferable, this doesn't seem
-#     easily possible due to some differences between enums and standard classes.
-#     See for example the following discussion: https://stackoverflow.com/questions/56131308/create-an-abstract-enum-class
-#     """
-#     pass
+# Common supertype for all data formats
+# =====================================
+class BaseDataFormat(ABC):
+    """
+    This class provides a supertype for all the concrete data formats. We don't
+    use any direct inheritance since they don't need to share any attributes or
+    methods. Instead, we simply register each concrete data formats (implemented
+    as Enums) after creation.
+    """
 
-# class StructuredDataFormat(BaseDataFormat):
-#     PD_DATAFRAME = pd.DataFrame
-#     NP_ARRAY = np.ndarray
-#     # PARQUET = "parquet"
-#     # JSONL = "jsonl"
-
-# class SemiStructuredDataFormat(BaseDataFormat):
-#     JSONL = "jsonl"
-
-# class FileFormat(BaseDataFormat):
-#     PATH = "path"
-#     PATH_LIST = "path_list"
-
-# StructuredDataFormat: TypeAlias = pd.DataFrame | np.ndarray
-# SemiStructuredDataFormat: TypeAlias = list[dict]
-
-
-from oo_ml.interface.data.data_set_type import DataSetType, StructuredData
-
-class DataFormat(Generic[DataSetType]):
     pass
 
-StructuredDataFormat = DataFormat[StructuredData]
+
+# Structured data
+# ===============
+class StructuredDataFormat(Enum):
+    PD_DATAFRAME = pd.DataFrame
+    NP_ARRAY = np.ndarray
+    # PARQUET = "parquet"
+    # JSONL = "jsonl"
+
+
+BaseDataFormat.register(StructuredDataFormat)
+
+
+# Semi-structured data
+# ====================
+class SemiStructuredDataFormat(Enum):
+    JSONL = "jsonl"
+
+
+BaseDataFormat.register(SemiStructuredDataFormat)
+
+
+# File data
+# =========
+class FileFormat(Enum):
+    PATH = "path"
+    PATH_LIST = "path_list"
+
+
+BaseDataFormat.register(FileFormat)
