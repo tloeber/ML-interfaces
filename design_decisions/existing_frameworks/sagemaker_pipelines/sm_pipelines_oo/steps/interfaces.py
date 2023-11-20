d5b529038175cb225d5b56354911d7c92508c1e7
tloeber <thomas.loeber73@gmail.com>
2023-11-20 09:29:17 -0500

    Move SagemakerPipelines abstraction to dedicated repo.
    Located at https://github.com/tloeber/sagemaker-pipelines-abstraction. (Split off using git-filter-repo.)

--- design_decisions/existing_frameworks/sagemaker_pipelines/sm_pipelines_oo/steps/interfaces.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-from sagemaker.workflow.steps import ProcessingStep
-
-class StepFactoryInterface(ABC):
-    @abstractmethod
-    def create_step(self, shared_config) -> ProcessingStep:
-        ...
--- design_decisions/existing_frameworks/sagemaker_pipelines/terraform/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-# Used in bucket name, so use "-" instead of "_", etc.
-# todo: find validator (like in pydantic)
-project_name = "example"
-region       = "us-east-1"
